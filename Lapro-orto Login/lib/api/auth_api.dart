5a7059f7a555e6d83450fd118e7479153892ae76
Izzaxx <luisangelizaguirrehernandez@gmail.com>
2021-01-23 22:27:18 -0700

    [UPDATE] Autenticacion desactivada

--- Lapro-orto Login/lib/api/auth_api.dart
+++ Lapro-orto Login/lib/api/auth_api.dart
@@ -1,17 +1,17 @@
-import 'package:sky_engine/_http/http.dart' as http;
-import 'package:meta/meta.dart';
-import '../app_config.dart';
+// import 'package:sky_engine/_http/http.dart' as http;
+// import 'package:meta/meta.dart';
+// import '../app_config.dart';
 
-class AuthApi{
+// class AuthApi{
 
-  registro(String username, String email, String password){
-    final url = "${AppConfig.apiHost}/registro";
+//   registro(String username, String email, String password){
+//     final url = "${AppConfig.apiHost}/registro";
 
-    http.post(url,body: {
-      "username":username,
-      "email":email,
-    });
+//     http.post(url,body: {
+//       "username":username,
+//       "email":email,
+//     });
 
-  }
+//   }
 
-}
+// }
